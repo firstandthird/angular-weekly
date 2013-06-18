b832363608ef2b32490688094d3b390efa678e0d
Greg Allen <me@jga.me>
2013-06-18 12:11:22 -0700

    [example] updated example

--- example/app.js
+++ example/app.js
@@ -2,4 +2,40 @@ angular.module('example-app', ['weekly']);
 
 var MainController = function($scope) {
 
+  var today = new Date();
+  var event1 = {
+    start: new Date(today.getFullYear(), today.getMonth(), today.getDate(), 10),
+    end: new Date(today.getFullYear(), today.getMonth(), today.getDate(), 12),
+    id: '123'
+  }
+  var event2 = {
+    start: new Date(today.getFullYear(), today.getMonth(), today.getDate(), 13),
+    end: new Date(today.getFullYear(), today.getMonth(), today.getDate(), 15),
+    id: '456'
+  }
+  var event3 = {
+    start: new Date(today.getFullYear(), today.getMonth(), today.getDate(), 16),
+    end: new Date(today.getFullYear(), today.getMonth(), today.getDate(), 17),
+    id: '456'
+  }
+
+  $scope.events = [event1, event2];
+
+  $scope.addEvent = function(evnt) {
+    console.log('event added', evnt);
+  }
+
+  $scope.removeEvent = function(evnt) {
+    console.log('event removed', evnt);
+  }
+
+  $scope.addAnotherEvent = function() {
+    $scope.events.push(event3);
+  }
+
+  $scope.$watch('events', function() {
+    console.log('events changed', arguments);
+  }, true);
+
+
 };
